80b39b0a65dc740f2c829744276782cc5b409a58
mark <mark.vanleijenhorst@foribus.nl>
2023-05-31 21:40:44 +0200

    Implement encryption

---
+++ ImitationGame.Core/Encryption/AsciiAlgoritme.cs
@@ -0,0 +1,33 @@
+﻿namespace ImitationGame.Core.Encryption;
+
+public class AsciiAlgoritme : IAlgoritme
+{
+    public string Encrypt(string message, int key)
+    {
+        var encryptedMessage = string.Empty;
+        foreach (var character in message.ToCharArray())
+        {
+            var numberCharacter = (int)character;
+            encryptedMessage += (numberCharacter + key).ToString("D5");
+        }
+
+        return encryptedMessage;
+    }
+
+
+    public string Decrypt(string message, int key)
+    {
+        var decryptedMessage = string.Empty;
+        var text = message;
+        while (text.Length > 5)
+        {
+            var sub = text[..5]; 
+            text = text[5..]; 
+            var number = int.Parse(sub) - key;
+
+            decryptedMessage += (char)number;
+        }
+
+        return decryptedMessage;
+    }
+}
--- ImitationGame.Core/Encryption/SwapLetterAlgoritme.cs
+++ ImitationGame.Core/Encryption/SwapLetterAlgoritme.cs
@@ -1,21 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ImitationGame.Core.Encryptie;
 
-namespace ImitationGame.Core.Encryption
+public class SwapLetterAlgoritme : IAlgoritme
 {
-    internal class SwapLetterAlgoritme : IAlgoritme
+    private const int Letter_A = 65;
+    private const int Letter_Z = 90;
+    private const int Letter_a = 97;
+    private const int Letter_z = 122;
+    private const int Number_0 = 48;
+    private const int Number_9 = 57;
+
+    public string Decrypt(string message, int key)
     {
-        public string Decrypt(string message, int key)
+        var encryptedMessage = string.Empty;
+        foreach (var character in message.ToCharArray())
         {
-            throw new NotImplementedException();
+            var index = (int)character;
+            bool isLower = index >= Letter_a && index <= Letter_z;
+            bool isUpper = index >= Letter_A && index <= Letter_Z;
+            bool isNumber = index >= Number_0 && index <= Number_9;
+
+            if (isLower)
+            {
+                encryptedMessage += SwapLetter(key, index, Letter_a, Letter_z);
+            }
+            else if (isUpper)
+            {
+                encryptedMessage += SwapLetter(key, index, Letter_A, Letter_Z);
+            }
+            else if (isNumber)
+            {
+                encryptedMessage += SwapLetter(key, index, Number_0, Number_9);
+            }
+            else
+            {
+                encryptedMessage += character;
+            }
         }
 
-        public string Encrypt(string message, int key)
+        return encryptedMessage;
+    }
+
+    public string Encrypt(string message, int key)
+    {
+        var decryptedMessage = string.Empty;
+        foreach (var character in message.ToCharArray())
         {
-            throw new NotImplementedException();
+            var index = (int)character;
+            bool isLower = index >= Letter_a && index <= Letter_z;
+            bool isUpper = index >= Letter_A && index <= Letter_Z;
+            bool isNumber = index >= Number_0 && index <= Number_9;
+
+            if (isLower)
+            {
+                decryptedMessage += SwapLetter(key * -1, index, Letter_a, Letter_z);
+            }
+            else if (isUpper)
+            {
+                decryptedMessage += SwapLetter(key * -1, index, Letter_A, Letter_Z);
+            }
+            else if (isNumber)
+            {
+                decryptedMessage += SwapLetter(key * -1, index, Number_0, Number_9);
+            }
+            else
+            {
+                decryptedMessage += character;
+            }
         }
+
+        return decryptedMessage;
+    }
+
+
+
+    private static char SwapLetter(int key, int index, int beginIndex, int endIndex)
+    {
+        var result = index + key;
+        if (result > endIndex)
+        {
+            return (char)(result - endIndex + beginIndex - 1);
+        }
+        else if (result < beginIndex)
+        {
+            return (char)(result - beginIndex + endIndex + 1);
+        }
+
+        return (char)result;
     }
 }
